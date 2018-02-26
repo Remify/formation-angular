cce4079b42066e4162d79b21252dd0d0a7727550
Rémi B <remibouguermouh@gmail.com>
2018-02-26 20:44:56 +0100

    [do] session 1.7

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Todo } from './models/todo';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo app';
+  aTodo: Todo = new Todo('Hello World');
 }
